06ba662f3f0c121a9f65975152eac4df80e668f7
ChanVimean <mrrmeanvlogger@gmail.com>
2025-02-07 14:38:19 +0700

    Adjust

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
-const jsonServer = require('json-server');
-const cors = require('cors');
-const dotenv = require('dotenv');
+const jsonServer = require('json-server')
+const cors = require('cors')
+const dotenv = require('dotenv')
 
-dotenv.config();
+dotenv.config()
 
-const server = jsonServer.create();
-const middleware = jsonServer.defaults();
-const route = jsonServer.router('data.json');
+const server = jsonServer.create()
+const middleware = jsonServer.defaults()
+const route = jsonServer.router('data.json')
 
-const PORT = process.env.PORT || 8075;
-const HOST = process.env.HOST || '127.0.0.1';
+const PORT = process.env.PORT || 8075
+const HOST = process.env.HOST || '127.0.0.1'
 
-server.use(cors());
-server.use(middleware);
-server.use(route);
+server.use(cors())
+server.use(middleware)
+server.use(route)
 
 server.listen(PORT, () => {
-  console.log(`✅ JSON Server is running on http://${HOST}:${PORT}`);
-});
+  console.log(`✅ JSON Server is running on http://${HOST}:${PORT}`)
+})
+
+
